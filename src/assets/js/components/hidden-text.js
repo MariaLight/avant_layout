1e13ef715e1f8d9695cafad85aa4fcbcc029d163
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-07-11 20:48:02 +1200

    add blocks

---
+++ src/assets/js/components/hidden-text.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const textBlockButton = document.querySelector('.text-block__button');
+    if (textBlockButton) {
+        textBlockButton.addEventListener('click', () => {
+            textBlockButton.nextElementSibling.classList.toggle('hidden');
+            textBlockButton.classList.toggle('active');
+        });
+    }
+
+    var details = document.querySelectorAll("details");
+    for (i = 0; i < details.length; i++) {
+        details[i].addEventListener("toggle", accordion);
+    }
+    function accordion(event) {
+        if (!event.target.open) return;
+        var details = event.target.parentNode.children;
+        for (i = 0; i < details.length; i++) {
+            if (details[i].tagName != "DETAILS" ||
+                !details[i].hasAttribute('open') ||
+                event.target == details[i]) {
+                continue;
+            }
+            details[i].removeAttribute("open");
+        }
+    }
+});
