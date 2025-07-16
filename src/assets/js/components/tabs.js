e17f16a754726c243ee914d42ea5ec0ceb832f20
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-07-17 02:13:56 +1200

    add service page

--- src/assets/js/components/tabs.js
+++ src/assets/js/components/tabs.js
@@ -1,13 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btns = document.querySelectorAll('.tabs__btn');
-    const tabs = document.querySelectorAll('.tabs__tab');
-    btns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            btns.forEach(b => b.classList.remove('active'));
-            tabs.forEach(tab => tab.classList.remove('active'));
-            btn.classList.add('active');
-            const tabName = btn.getAttribute('data-tab');
-            document.querySelector('.tabs__tab[data-tab-content="' + tabName + '"]').classList.add('active');
+document.addEventListener('DOMContentLoaded', function () {
+
+    const allTabs = document.querySelectorAll('.tabs');
+    allTabs.forEach(tab => {
+        const btns = tab.querySelectorAll('.tabs__btn');
+        const tabs = tab.querySelectorAll('.tabs__tab');
+        btns.forEach(btn => {
+            btn.addEventListener('click', function () {
+                btns.forEach(b => b.classList.remove('active'));
+                tabs.forEach(tab => tab.classList.remove('active'));
+                btn.classList.add('active');
+                const tabName = btn.getAttribute('data-tab');
+                document.querySelector('.tabs__tab[data-tab-content="' + tabName + '"]').classList.add('active');
+            });
         });
-    });
+    })
+
 });
